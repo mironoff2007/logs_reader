aedd83f5e2b2a1c9e3fca2d58a89b3d1024965d4
mironoff2007 <mironoff2007@rambler.ru>
2020-09-22 21:43:56 +0300

    Logs separation

--- logs_reader.py
+++ logs_reader.py
@@ -1,41 +1,50 @@
 import csv
 import math
-with open(r'C:\2020-08-20 10-59-12_target.csv',  newline='') as f:
+with open(r'C:\Users\Jura\Desktop\2019_11_15_flight_logs\VOLK\2020-08-20_10-59-45_2.csv',  newline='') as f:
     #C:\Users\Jura\Desktop\2019_11_15_flight_logs\VOLK\2020-08-20_10-59-45.csv
     #2020-08-20 10-59-12_target.csv
     reader = csv.reader(f, delimiter=';')
     your_list = list(reader)
-    list_row=[]
-    x_coord=0.0;
-    y_coord=0.0
-    is_first=False
+    list_row = []
+    x_coord = 0.0;
+    y_coord = 0.0
+    is_not_first = False
     aim = False
-    lat1=0.0 #y0
-    lat2=0.0
-    lon1=0.0 #x0
-    lon2=0.0
+    end_aim = False
+    lat1 = 0.0  # y0
+    lat2 = 0.0
+    lon1 = 0.0  # x0
+    lon2 = 0.0
     R = 6371000;
-    label=""
-    ch7=0;
-    alt=0
-#print(your_list[1])
-    i=0;
-#lat1=float(your_list[1][11])
-#lon1=float(your_list[1][12])
+    label = ""
+    ch7 = 0;
+    alt = 0
+    # print(your_list[1])
+    i = 0;
+    # lat1=float(your_list[1][11])
+    # lon1=float(your_list[1][12])
 
-for l in range (0, len(your_list)):
-    list_row=your_list[l]
-    if (your_list[l][0] == "RCIN") :
-        ch7=float(your_list[l][1+7])
-        if (ch7>1500):
-            #("Signal=",your_list[l][1+7])
-            aim=True
+for l in range(0, len(your_list)):
+    list_row = your_list[l]
 
-    if(your_list[l][0]=="GPS")and not is_first and aim:
-        is_first=True
-        lat1=float(your_list[l][7])
-        lon1=float(your_list[l][8])
-    elif(your_list[l][0]=="GPS") and is_first and aim:
+    if (your_list[l][0] == "RCIN"):
+        ch7 = float(your_list[l][1 + 7])
+    if (ch7 > 1600):
+    # print("Signal=",your_list[l][1+7])
+        aim = True
+    if((ch7 < 1600) and aim):
+        end_aim = True
+
+    if (ch7 > 1600 and end_aim):
+        print("next")
+        aim = False
+        end_aim = False
+    if (your_list[l][0] == "GPS")  and aim and not is_not_first:
+        is_not_first = True
+        lat1 = float(your_list[l][7])
+        lon1 = float(your_list[l][8])
+    if (your_list[l][0] == "GPS") and aim and is_not_first:
+
         lat2=float(your_list[l][7])
         lon2=float(your_list[l][8])
         alt=your_list[l][9]
@@ -73,7 +82,3 @@ for l in range (0, len(your_list)):
 
     i=i+1
 
-    if i == 1211:
-        print()
-
-    if i == 3000: break
