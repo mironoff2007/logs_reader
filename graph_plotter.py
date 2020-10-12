d2836794c3bde193d0f1450c1a1da06e27fdcea8
mironoff2007 <mironoff2007@rambler.ru>
2020-10-12 11:36:52 +0300

    Second graph plot added

--- graph_plotter.py
+++ graph_plotter.py
@@ -9,6 +9,15 @@ root = tk.Tk()
 root.withdraw()
 list_x=[]
 list_y=[]
+
+list_x2=[]
+list_y2=[]
+
+list_z1=[]
+list_z2=[]
+
+list_i=[]
+
 file_path = filedialog.askopenfilename()
 
 print(file_path)
@@ -21,10 +30,27 @@ with open(file_path, newline='') as f:
         list_x.append(float(row[0].replace(',','.')))
         list_y.append(float(row[1].replace(',','.')))
 
-    plt.plot(list_x,list_y)
+        list_x2.append(float(row[3].replace(',', '.')))
+        list_y2.append(float(row[4].replace(',', '.')))
 
+        list_z1.append(float(row[2].replace(',', '.')))
+        list_z2.append(float(row[5].replace(',', '.')))
+        list_i.append(i)
+
+    plt.plot(list_x,list_y,label="hunter")
+    plt.plot(list_x2,list_y2,label="target")
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',ncol=2, mode="expand", borderaxespad=0.)
     #plt.yticks(np.arange(100, 170, 5))
-    plt.ylabel('some numbers')
+    plt.ylabel('XY coordinates')
+    plt.figure(0)
+
+    plt.plot(list_i,list_z1,label="hunter")
+    plt.plot(list_i, list_z2, label="target")
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left', ncol=2, mode="expand", borderaxespad=0.)
+    plt.ylabel('Altitude')
+    plt.figure(1)
+
+
     plt.show()
 
 def printgr(x,y):
