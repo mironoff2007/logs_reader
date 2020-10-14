eb785243f07e04b71f85931126e1d59f4f40d166
mironoff2007 <mironoff2007@rambler.ru>
2020-10-14 18:03:44 +0300

    Refactoring

--- logs_reader.py
+++ logs_reader.py
@@ -1,19 +1,36 @@
 import csv
-import math
-
 import coordinates_converter as Converter
+
 class Logs_reader:
 
 
     def __init__(self):
         self.lon0 = 0.0
         self.lat0 = 0.0
-        self.time0 = 0.0
+
+        self.lon1_hun = 0.0  # x0
+        self.lon2_hun = 0.0
+
+        self.lon1_tar = 0.0  # x0
+        self.lon2_tar = 0.0
+
+        self.lat1_hun = 0.0  # y0
+        self.lat2_hun = 0.0
+
+        self.lat1_tar = 0.0  # y0
+        self.lat2_tar = 0.0
+
+        self.alt_tar=0
+        self.alt_hun=0
+
         self.x_coord0=0
         self.y_coord0=0
         self.lon_obj=0
         self.lat_obj=0
         self.sect=0
+
+        self.t0=0
+
         self.coord_conv=Converter.Coordinates_converter
 
     def setCoord0(self,lon_obj,lat_obj):
@@ -101,144 +118,124 @@ class Logs_reader:
             file_target=open(s_target,'r')
             target_reader=csv.reader(file_target, delimiter=';')
             target_lines=list(target_reader)
-            target_lines_number=len(target_lines)
 
             reader = csv.reader(f, delimiter=';')
-            your_list = list(reader)
+            hunter_list = list(reader)
 
-            hunter_lines_number=len(your_list)
-            list_row = []
+            hunter_lines_number=len(hunter_list)
+            target_lines_number = len(target_lines)
+
             x_coord = 0.0
             y_coord = 0.0
             x_coord_tar = 0.0
             y_coord_tar = 0.0
             t=0.0
-            t0=0.0
+
             mark=0
 
             is_not_first = False
             aim = False
             end_aim = False
-            lat1 = 0.0  # y0
-            lat2 = 0.0
-            lon1 = 0.0  # x0
-            lon2 = 0.0
-            lat1_tar = 0.0  # y0
-            lat2_tar = 0.0
-            lon1_tar = 0.0  # x0
-            lon2_tar = 0.0
-            R = 6371000
-            label = ""
-            ch7 = 0;
-            alt = 0
-            alt_tar=0
-            # print(your_list[1])
+
+            self.alt_hun = 0
+            self.alt_tar=0
+
             i = 0;
-            # lat1=float(your_list[1][11])
-            # lon1=float(your_list[1][12])
+
 
         self.sect=1
         f_write = open(r'D:\section_1.csv', "w")
-        f_write_gps = open(r'D:\section_1_gps.csv', "w")
 
-        ch=8
-        for l in range(0, min(len(your_list),len(target_lines)-1)):
-            row_hunter = your_list[l]
+        ch=8#aim channel
+        for l in range(0, min(len(hunter_list),len(target_lines)-1)):
+            row_hunter = hunter_list[l]
             row_target = target_lines[l]
-            sect=1
 
-            if (your_list[l][0] == "RCIN"):
-                ch_v = float(your_list[l][1 + ch])
+            if (hunter_list[l][0] == "RCIN"):
+                ch_v = float(hunter_list[l][1 + ch])
             if  (ch_v > 1600):
-            # print("Signal=",your_list[l][1+7])
+
                 aim = True
             if((ch_v < 1600) and aim):
-                end_aim = True
 
-            if (ch_v > 1600 and end_aim):
                 print("next")
                 aim = False
-                end_aim = False
+
                 self.sect = self.sect + 1
 
                 f_write.close()
                 f_write = open(r'D:\section_' + str(self.sect)+'.csv', "w")
 
-                f_write_gps.close()
-                f_write_gps = open(r'D:\section_' + str(self.sect) + '_gps.csv', "w")
-
-                t0=float(your_list[l][1])/1000000
+                self.t0=float(hunter_list[l][1])/1000000
 
                 is_not_first=False
 
-
-
             #logs fist line
-            if (your_list[l][0] == "GPS")  and aim and not is_not_first:
+            if (hunter_list[l][0] == "GPS")  and aim and not is_not_first:
                 is_not_first = True
-                lat1 = float(your_list[l][7])
-                lon1 = float(your_list[l][8])
 
-                lat1_tar = float(target_lines[l][7])
-                lon1_tar = float(target_lines[l][8])
+                self.lat1_hun = float(hunter_list[l][7])
+                self.lon1_hun = float(hunter_list[l][8])
 
-                self.time0=int (your_list[l][1])
-
-                mark = int(your_list[l][1])
-                t0=float(your_list[l][1])/1000000
+                self.lat1_tar = float(target_lines[l][7])
+                self.lon1_tar = float(target_lines[l][8])
 
                 x_coord_tar = round(0 + self.x_coord0, 3)
                 y_coord_tar = round(0 + self.y_coord0, 3)
-                alt_tar = "{:.2f}".format(float(target_lines[l][9]))
-                alt = "{:.2f}".format(float(your_list[l][9]))
+
+                self.alt_tar = "{:.2f}".format(float(target_lines[l][9]))
+                self.alt_hun = "{:.2f}".format(float(hunter_list[l][9]))
+
+                #time
+                mark = int(hunter_list[l][1])
+                self.t0 = float(hunter_list[l][1]) / 1000000
+
                 s1 = ''
-                s1 = str(0).replace('.', ',') + ";" + str(0).replace('.', ',') + ";" + str(alt).replace('.',',') + ";" + str(x_coord_tar).replace('.', ',') + ";" + str(y_coord_tar).replace('.', ',') + ";" + str(alt_tar).replace('.', ',') + ";" + str(0).replace('.', ',') + ";" + str(mark)
+                s1 = str(0).replace('.', ',') + ";" + str(0).replace('.', ',') + ";" + str(self.alt_hun).replace('.',',') + ";" + str(x_coord_tar).replace('.', ',') + ";" + str(y_coord_tar).replace('.', ',') + ";" + str(self.alt_tar).replace('.', ',') + ";" + str(0).replace('.', ',') + ";" + str(mark)
                 f_write.write(s1)
                 f_write.write('\n')
 
-
-            elif (your_list[l][0] == "GPS") and aim and is_not_first:
+            elif (hunter_list[l][0] == "GPS") and aim and is_not_first:
             #hunter
-                lat2=float(your_list[l][7])
-                lon2=float(your_list[l][8])
-                alt="{:.2f}".format(float(your_list[l][9]))
+                self.lat2_hun=float(hunter_list[l][7])
+                self.lon2_hun=float(hunter_list[l][8])
+                self.alt="{:.2f}".format(float(hunter_list[l][9]))
 
-                t_cur=float(your_list[l][1])/1000000
-                t=t_cur-t0
-                mark = int(your_list[l][1])
+                t_cur=float(hunter_list[l][1])/1000000
+                t=round(t_cur-self.t0,3)
 
-                d_x = self.coord_conv.CalcXCoord(lon1,lat1,lon2,lat2)
-                d_y = self.coord_conv.CalcYCoord(lon1,lat1,lon2,lat2)
+                mark = int(hunter_list[l][1])
+
+                d_x = self.coord_conv.CalcXCoord(self.lon1_hun,self.lat1_hun,self.lon2_hun,self.lat2_hun)
+                d_y = self.coord_conv.CalcYCoord(self.lon1_hun,self.lat1_hun,self.lon2_hun,self.lat2_hun)
 
                 x_coord=round(x_coord+d_x,3)
                 y_coord=round(y_coord+d_y,3)
 
 
             #target
-                lat2_tar = float(target_lines[l][7])
-                lon2_tar = float(target_lines[l][8])
-                alt_tar ="{:.2f}".format(float(target_lines[l][9]))
+                self.lat2_tar = float(target_lines[l][7])
+                self.lon2_tar = float(target_lines[l][8])
+                self.alt_tar ="{:.2f}".format(float(target_lines[l][9]))
 
-                d_x_tar = self.coord_conv.CalcXCoord(lon1_tar, lat1_tar, lon2_tar, lat2_tar)
-                d_y_tar = self.coord_conv.CalcYCoord(lon1_tar, lat1_tar, lon2_tar, lat2_tar)
+                d_x_tar = self.coord_conv.CalcXCoord(self.lon1_tar, self.lat1_tar, self.lon2_tar, self.lat2_tar)
+                d_y_tar = self.coord_conv.CalcYCoord(self.lon1_tar, self.lat1_tar, self.lon2_tar, self.lat2_tar)
 
                 x_coord_tar = round(x_coord_tar + d_x_tar + self.x_coord0,3)
                 y_coord_tar = round(y_coord_tar + d_y_tar + self.y_coord0,3)
+
                 s1=''
-                s1=str(x_coord).replace('.',',')+ ";"+ str(y_coord).replace('.',',')+ ";"+ str(alt).replace('.',',')+";"+str(x_coord_tar).replace('.',',')+ ";"+str(y_coord_tar).replace('.',',')+";"+ str(alt_tar).replace('.',',')+";"+str(t).replace('.', ',')+";"+str(mark)
-                s2 = ''
-                s2 = str(lon2).replace('.', ',') + ";" + str(lat2).replace('.', ',') + ";" +  str(alt).replace('.',',')+";"+str(lon2_tar).replace('.',',')+ ";"+str(lat2_tar).replace('.',',')+";"+ str(alt_tar).replace('.',',')
+                s1=str(x_coord).replace('.',',')+ ";"+ str(y_coord).replace('.',',')+ ";"+ str(self.alt_hun).replace('.',',')+";"+str(x_coord_tar).replace('.',',')+ ";"+str(y_coord_tar).replace('.',',')+";"+ str(self.alt_tar).replace('.',',')+";"+str(t).replace('.', ',')+";"+str(mark)
 
                 print(s1)
                 f_write.write(s1)
                 f_write.write('\n')
 
-
-                lon1_tar=lon2_tar
-                lat1_tar=lat2_tar
-                lon1 = lon2
-                lat1 = lat2
-    #print( round(d_x,3), round(d_y,3))
+                self.lon1_tar = self.lon2_tar
+                self.lat1_tar = self.lat2_tar
+                self.lon1_hun = self.lon2_hun
+                self.lat1_hun = self.lat2_hun
+    
 
             i=i+1
 
